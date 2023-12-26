95eff717bec6e9065f1fa60cfc2aefaa601381b1
Yandex Student Vasiliy <chipmunkzombie@gmail.com>
2023-12-26 20:53:06 +0400

    merge bs into main

---
+++ BS.pas
@@ -0,0 +1,195 @@
+Program BlackSmithy;
+{$Include 'all.inc'}
+
+var
+  TimeStart : TDateTime;
+
+const
+  // Containers setup:
+  CharBackpack = $5D2A576B;
+  LegendaryBag = $5E66B128;
+  PerfectBag = $5E66B1F7;
+  ExceptionalBag = $5E66B301;
+
+  // Item to craft:
+  // ItemType = $1401; // Kriss
+  ItemType = $13FE; // Katana
+
+  // Tools and Objects:
+  Ingots = $1BF2;
+  ForgeObj = $5E293A96;
+  HammerType = $13E3;
+  TongsType = $0FBB;
+  SmeltExcept = 1; // 1 = smelt exceptional items into ingots, 0 = store exceptional items in the ExceptionalBag
+
+  MAX_EXCEPTIONAL_COUNT = 999;  // Adjust the maximum count as needed
+  MAX_PERFECT_COUNT = 999;      // Adjust the maximum count as needed
+  MAX_LEGENDARY_COUNT = 999;    // Adjust the maximum count as needed
+
+procedure Resmelt;
+begin
+  findtype(ItemType, backpack);
+  while FindCount() > 0 do
+  begin
+    Clearjournal();
+    UseObject(FindType(TongsType, Backpack));
+    wait(500);
+    findtype(ItemType, backpack);
+    WaitTargetObject(finditem);
+    wait(1000);
+    WaitTargetObject(ForgeObj);
+    wait(3000);
+  end;
+end;
+
+procedure CheckStopConditions;
+begin
+  FindType(ItemType, ExceptionalBag);
+  if FindCount >= MAX_EXCEPTIONAL_COUNT then
+  begin
+    AddToSystemJournal('Reached maximum exceptional count. Stopping script.');
+    Halt;
+  end;
+
+  FindType(ItemType, PerfectBag);
+  if FindCount >= MAX_PERFECT_COUNT then
+  begin
+    AddToSystemJournal('Reached maximum perfect count. Stopping script.');
+    Halt;
+  end;
+
+  FindType(ItemType, LegendaryBag);
+  if FindCount >= MAX_LEGENDARY_COUNT then
+  begin
+    AddToSystemJournal('Reached maximum legendary count. Stopping script.');
+    Halt;
+  end;
+end;
+
+procedure CraftItem;
+begin
+  UseObject(ObjAtLayerEx(RhandLayer, self));
+  findtype(Ingots, backpack);
+  WaitTargetObject(finditem);
+  wait(1000);
+  WaitGump('300'); // Change For item type
+  wait(500);
+  WaitGump('3010');
+  WaitGump('0x' + IntToHex(ItemType, 4)); 
+  wait(500);
+
+  repeat
+    wait(500);
+  until (InJournalBetweenTimes('stop', TimeStart, Now) <> -1) or (InJournal('Cancelled') <> -1);
+
+  wait(100);
+
+  if InJournal('Legendary') > -1 then
+  begin
+    CheckStopConditions;
+    UoSay('Finally the Masterpiece! (Legendary)');
+    ClearJournal;
+    Findtype(ItemType, backpack);
+    wait(100);
+    MoveItem(finditem, 0, LegendaryBag, 0, 0, 0);
+    wait(100);
+  end;
+
+  if InJournal('Perfect') > -1 then
+  begin
+    CheckStopConditions;
+    UoSay('That one looks really good! (Perfect)');
+    ClearJournal;
+    Findtype(ItemType, backpack);
+    wait(100);
+    MoveItem(finditem, 0, PerfectBag, 0, 0, 0);
+    wait(100);
+  end;
+
+  if InJournal('Exceptional') > -1 then
+  begin
+    UoSay('This one is not bad! (Exceptional)');
+
+    if SmeltExcept = 0 then
+    begin
+      CheckStopConditions;
+      ClearJournal;
+      wait(500);
+      Findtype(ItemType, backpack);
+      wait(100);
+      MoveItem(finditem, 0, ExceptionalBag, 0, 0, 0);
+      wait(100);
+    end
+    else if SmeltExcept = 1 then
+    begin
+      ClearJournal;
+      wait(500);
+      Findtype(ItemType, backpack);
+      wait(100);
+      MoveItem(finditem, 0, CharBackpack, 0, 0, 0);
+      wait(100);
+    end;
+  end;
+
+  if InJournal('Success') <> -1 then
+  begin
+    wait(500);
+    Findtype(ItemType, backpack);
+    wait(100);
+    MoveItem(finditem, 0, CharBackpack, 0, 0, 0);
+    wait(100);
+  end;
+end;
+
+procedure CheckHammer;
+begin
+  if (ObjAtLayerEx(RhandLayer, self) = 0) then
+  begin
+    UseObject(FindType(HammerType, Backpack));
+    UoSay('Another hammer cracked!');
+  end;
+end;
+
+procedure CheckCounts;
+var
+  itemCount: Integer;
+begin
+  AddToSystemJournal('***');
+
+  FindType(ItemType, LegendaryBag);
+  itemCount := FindCount;
+  AddToSystemJournal('Legendary: ' + IntToStr(itemCount));
+  Wait(100);
+  FindType(ItemType, PerfectBag);
+  itemCount := FindCount;
+  AddToSystemJournal('Perfect: ' + IntToStr(itemCount));
+  Wait(100);
+  FindType(ItemType, ExceptionalBag);
+  itemCount := FindCount;
+  if SmeltExcept = 0 then
+    AddToSystemJournal('Exceptional: ' + IntToStr(itemCount));
+  Wait(100);
+  FindType(HammerType, Backpack);
+  itemCount := FindCount;
+  AddToSystemJournal('Hammers: ' + IntToStr(itemCount));
+  Wait(100);
+  FindType(TongsType, Backpack);
+  itemCount := FindCount;
+  AddToSystemJournal('Tongs: ' + IntToStr(itemCount));
+  Wait(100);
+  FindType(Ingots, Backpack);
+  itemCount := FindFullQuantity;
+  AddToSystemJournal('Ingots:' + IntToStr(itemCount));
+end;
+
+Begin //the main loop
+  while (not Dead) do
+  begin
+    CancelMenu;
+    ClearJournal();
+    CheckHammer;
+    CraftItem;
+	Resmelt;
+    CheckCounts;
+  end;
+End.
---
+++ Mining_Minoc_unload.pas
@@ -0,0 +1,213 @@
+Program mining;
+
+{$Include 'all.inc'}
+
+type
+  LumbRecord = Record
+    x, y, tt: integer;
+  end;
+
+var
+  x, y, k: integer;
+  ctime: TDateTime;
+  mn1, mf1, mf2, ms1, ms2: Integer;
+
+const
+  Pickaxe1 = $0E85;
+  Pickaxe2 = $0E85;
+  IngotType = $1BF2;
+
+  typeTTools = $1EBA;
+
+  BankX = 2509; //Minoc Bank coordinates
+  BankY = 537;
+
+  IngotChest = $5E4AB377; //ID of a container inside bank to store ingots in
+
+function CheckPickaxe: Boolean;
+var
+  tmpser: Cardinal;
+begin
+  Result := true;
+  if (ObjAtLayerEx(RhandLayer, self) = 0) then
+  begin
+    tmpser := findtype(Pickaxe1, backpack);
+    if tmpser = 0 then
+      tmpser := findtype(Pickaxe2, backpack);
+    if tmpser = 0 then
+    begin
+      Result := false;
+      exit;
+    end;
+    if not equip(RhandLayer, tmpser) then
+    begin
+      wait(1000);
+      if not equip(RhandLayer, tmpser) then
+      begin
+        Result := false;
+        exit;
+      end;
+    end;
+    wait(500);
+    checksave;
+  end;
+end;
+
+procedure CheckPick;
+begin
+  CheckSave;
+  if (Count($0E85) < 1) then
+  begin
+    repeat
+      ClearJournal;
+      WaitTargetobject(findtype($1BF2, Backpack));
+      UseObject(FindType($1EBA, backpack));
+      wait(500);
+      waitgump('400');
+      wait(500);
+      waitgump('0x0E85');
+      WaitJournalLine(Now, 'Success|destroy|You put', 10000);
+      wait(500);
+    until (Count($0E85) >= 2);
+  end;
+end;
+
+procedure MiningAround;
+Begin
+  for x := -0 to 0 do
+  begin
+    for y := -0 to 0 do
+    begin
+      repeat
+        waitconnection(5000);
+        if not CheckPickaxe then
+          exit;
+        if TargetPresent then
+          CancelTarget;
+        if WarMode = true then
+          SetWarMode(false);
+        ctime := Now;
+        WaitTargetObject(self);
+        UseObject(ObjAtLayerEx(RhandLayer, self));
+        wait(250);
+        k := 0;
+        repeat
+          wait(100);
+          k := k + 1;
+          //checksave;
+          mn1 := InJournalBetweenTimes('ou can|stop|far', ctime, Now);
+          mf1 := InJournalBetweenTimes('already', ctime, Now);
+          mf2 := InJournalBetweenTimes('fail', ctime, Now);
+          ms1 := InJournalBetweenTimes('way', ctime, Now);
+          ms2 := InJournalBetweenTimes('mine or dig|There is no metal here to mine|for sand somewhere', ctime, Now);
+        until (mn1 <> -1) or (mf1 <> -1) or (mf2 <> -1) or (ms1 <> -1) or (ms2 <> -1) or (k > 300) or dead;
+
+      until (ms2 <> -1) or dead;
+    end;
+  end;
+End;
+
+procedure DropMap;
+begin
+  repeat
+    FindTypeEx($14ED, $ffff, backpack, False);
+    if (findcount > 0) then
+      Drop(finditem, 0, 0, 0, 0);
+    wait(100);
+    checksave;
+  until findcount = 0;
+end;
+
+procedure randomstep();
+var
+  ld: Integer;
+begin
+  ld := Random(4);
+
+  if (ld = 0) then
+  begin
+    Step(0, false);
+    Step(0, false);
+  end;
+
+  if (ld = 1) then
+  begin
+    Step(2, false);
+    Step(2, false);
+  end;
+
+  if (ld = 2) then
+  begin
+    Step(4, false);
+    Step(4, false);
+  end;
+
+  if (ld = 3) then
+  begin
+    Step(6, false);
+    Step(6, false);
+  end;
+end;
+
+procedure goUpload;
+label
+  m, g;
+Begin
+  AddToSystemJournal('Smelting ore into ingots...');
+  NewMoveXY(2573, 458, true, 1, false);
+m:
+  FindType($19B8, backpack);
+  If (FindItem > 0) then
+  begin
+    UseObject(FindItem);
+    wait(1000);
+    goto m;
+  end
+  else
+    ignore(finditem);
+
+  ignoreReset;
+
+  AddToSystemJournal('Backpack is full. Unloading into the bank...');
+  NewMoveXY(2563, 529, true, 1, true);
+  NewMoveXY(2510, 540, true, 1, true);
+  NewMoveXY(BankX, BankY, true, 1, true);
+  UoSay('bank');
+  wait(1000);
+
+  AddToSystemJournal('Storing Ingots into the bank...');
+
+g:
+  FindType($1BF2, backpack);
+  If finditem > 0 then
+  begin
+    moveItem(finditem, 0, IngotChest, 0, 0, 0);
+    wait(1000);
+    goto g;
+  end;
+
+  AddToSystemJournal('Go back to the mine.');
+
+  NewMoveXY(2563, 529, true, 1, true);
+  MoveXYZ(2562, 508, -40, 1, 0, true);
+  MoveXYZ(2564, 481, -45, 1, 0, true);
+end;
+
+Begin
+  SetARStatus(true);
+  while (connected) do
+  begin
+    while (true) do
+    begin
+      randomstep;
+      FindDistance := 2;
+      MiningAround;
+      DropMap;
+      CheckPick
+      If weight > (MaxWeight - 50) then
+        //weight left before unload
+        goUpload;
+      wait(3000);
+    end;
+  end;
+End.
